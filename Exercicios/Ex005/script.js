7316cdb99ec0f73ef08911615d90b066486901e4
Felipe dos Santos Toledo <felipinhobrx@gmail.com>
2026-02-22 17:58:29 -0300

    js

---
+++ Exercicios/Ex005/script.js
@@ -0,0 +1,9 @@
+var idade = 18
+
+if (idade >= 18) {
+    console.log ('Você é maior de idade')
+    console.log ('Pode entrar na festa!!!')
+} else {
+    console.log ('Você é menor de idade')
+    console.log ('Não pode entrar na festa!!!')
+}
