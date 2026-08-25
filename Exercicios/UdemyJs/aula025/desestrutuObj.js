d69b9fda70c06c99ad69584536fb6da68587b7f7
Felipe dos Santos Toledo <felipinhobrx@gmail.com>
2026-08-25 12:41:21 -0300

    new for

--- Exercicios/UdemyJs/aula025/desestrutuObj.js
+++ Exercicios/UdemyJs/aula025/desestrutuObj.js
@@ -1,14 +1,21 @@
 const pessoa = {
-    nome: 'Luiz',
+    //nome: 'arthur',
     sobrenome: 'Miranda',
     idade: 30,
-    endereço: {
+    endereco: {
         rua: 'Av Brasil',
         numero: 320
     }
 }
-
+/*
 console.log(pessoa.nome) 
-
-const {nome = 'Felipe'} = pessoa
+//             v-- Valor padrão caso não exista a variavel
+const {nome = 'Felipe'} = pessoa; 
 console.log(nome)
+*/
+/*
+const {endereco: {rua, numero}} = pessoa
+console.log(rua, numero)
+*/
+const {nome = 'Felipe', ...resto } = pessoa
+console.log(nome, resto)
