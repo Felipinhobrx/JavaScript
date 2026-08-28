813da9fb7cac902ed617cc53cd47cda6f29aa3ba
Felipe dos Santos Toledo <felipinhobrx@gmail.com>
2026-08-28 12:54:33 -0300

    new ex

---
+++ Exercicios/UdemyJs/aula026/for02.js
@@ -0,0 +1,22 @@
+const frutas = ['Pera', 'Maçã', 'Uva']
+
+/*
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+}
+*/
+/*
+for (let indice in frutas) {
+    console.log(indice, frutas[indice])
+}
+*/
+
+const pessoa = {
+    nome: 'Felipe',
+    sobrenome: 'Toledo',
+    idade: 16
+}
+
+for (let indice in pessoa) {
+    console.log(indice, pessoa[indice])
+}
---
+++ Exercicios/UdemyJs/aula026/for03.js
@@ -0,0 +1,16 @@
+const nome = 'Felipe'
+const nomes = ['Felipe', 'Sandal', 'Judeu']
+
+/*
+for (let valor of nome) {
+    console.log(valor)
+}
+*/
+
+for (let valor of nomes) {
+    console.log(valor)
+}
+
+nomes.forEach(function(valor) {
+    console.log(valor)
+})
