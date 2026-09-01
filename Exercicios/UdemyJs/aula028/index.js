c12b420b1ebce77690af58b4ed86383c958c139c
Felipe dos Santos Toledo <felipinhobrx@gmail.com>
2026-08-31 23:06:41 -0300

    new ex

---
+++ Exercicios/UdemyJs/aula028/index.js
@@ -0,0 +1,28 @@
+/*
+let i = 0
+
+while (i <= 10) {
+    console.log(i)
+    i++
+}
+*/
+
+function random(min, max) {
+    const r = Math.random() * (max - min) + min
+    return Math.floor(r)
+}
+
+let rand = random(1, 50)
+console.log(rand)
+
+while (rand !== 10) {
+    rand = random(1, 50)
+    console.log(rand)
+}
+
+console.log('Separação')
+
+do {
+    rand = random(1, 50)
+    console.log(rand)
+} while(rand !== 10)
