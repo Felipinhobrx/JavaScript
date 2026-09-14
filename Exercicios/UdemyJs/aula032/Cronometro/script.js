44e02372f2a5c10c9f224a02a1afc4cd3c1da6ac
Felipe dos Santos Toledo <felipinhobrx@gmail.com>
2026-09-14 20:00:23 -0300

    cronometro

--- Exercicios/UdemyJs/aula032/Cronometro/script.js
+++ Exercicios/UdemyJs/aula032/Cronometro/script.js
@@ -1,11 +1,38 @@
-const timerhtml = document.querySelector('.timer')
+function getSeconds(segundos) {
+    const data = new Date(segundos * 1000)
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    })
+}
 
-const data = new Date()
-let horas = data.toLocaleDateString()
+const relogio = document.querySelector('.relogio')
+const iniciar = document.querySelector('#iniciar')
+const pausar = document.querySelector('#pausar')
+const zerar = document.querySelector('#zerar')
+let segundos = 0
+let timer;
 
-function cronometro() {
-
-    setInterval(function() {
-    timerhtml.innerHTML = cronometro()
+function iniciaRelogio() {
+    timer = setInterval(function() {
+        segundos++
+        relogio.innerHTML = getSeconds(segundos)
     }, 1000)
 }
+
+iniciar.addEventListener('click', function(event) {
+    relogio.classList.remove('pausado')
+    clearInterval(timer)
+    iniciaRelogio()
+})
+
+pausar.addEventListener('click', function(event) {
+    clearInterval(timer)
+    relogio.classList.add('pausado')
+})
+
+zerar.addEventListener('click', function(event) {
+    clearInterval(timer)
+    relogio.innerHTML = '00:00:00'
+    segundos = 0
+})
